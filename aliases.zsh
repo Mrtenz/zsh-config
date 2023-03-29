bb9b76bc281f116cf3089050611b203b6fb84797
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-03-29 17:17:51 +0200

    Add an alias for the commit script
    An alias for the commit script was added for convenience with the command 'commit'.

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,9 @@
 # Alias for IntelliJ IDEA.
-alias idea="open -na \"IntelliJ IDEA.app\" --args
+alias idea="open -na \"IntelliJ IDEA.app\" --args"
 
 # Alias for "dev" script.
 # See: https://gist.github.com/Mrtenz/4b2961ea7c8d6466b4eebd0f3514f5d5.
 alias dev="source ~/Development/dev.sh"
+
+# Alias for "commit" script.
+alias commit="~/Development/commit.sh"
